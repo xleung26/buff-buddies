460b5e198082cb56e1fd67ef578deeee25c366c4
jmkchoi <jstnch24@gmail.com>
2019-03-19 10:08:43 -0700

    initial app structure

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import FindBuddyScreen from '../screens/FindBuddyScreen';
+import MessagesScreen from '../screens/MessagesScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const HomeStack = createStackNavigator({
+  Home: FindBuddyScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Find Buddy',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const MessagesStack = createStackNavigator({
+  Messages: MessagesScreen,
+});
+
+MessagesStack.navigationOptions = {
+  tabBarLabel: 'Messages',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+    />
+  ),
+};
+
+const ProfileStack = createStackNavigator({
+  Profile: ProfileScreen,
+});
+
+ProfileStack.navigationOptions = {
+  tabBarLabel: 'Profile',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  MessagesStack,
+  HomeStack,
+  ProfileStack,
+});
