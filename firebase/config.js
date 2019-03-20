257fbd73461ba382ecbaf1dde2b4ca6d0ff787c0
jmkchoi <jstnch24@gmail.com>
2019-03-20 10:36:50 -0700

    remove ignored files

--- firebase/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: 'AIzaSyBZTfggIR_6ZkFULP5Kmx0djfoWVZSHEDw',
-  authDomain: 'buff-buddies-5c788.firebaseapp.com',
-  databaseURL: 'https://buff-buddies-5c788.firebaseio.com',
-  projectId: 'buff-buddies-5c788',
-  storageBucket: 'buff-buddies-5c788.appspot.com',
-  messagingSenderId: '580897267631'
-};
