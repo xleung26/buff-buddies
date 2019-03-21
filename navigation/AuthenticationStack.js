00e1829bbf5d774d5c5ef0606de6d1f6c5d561cd
Anomali007 <franzese007@gmail.com>
2019-03-21 16:51:00 -0700

    Navigation stack to handle initial react navigation through login and signup.

--- navigation/AuthenticationStack.js
+++ navigation/AuthenticationStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from 'react-navigation';
 import LoginScreen from '../components/auth/LoginScreen.js';
 import SignupScreen from '../components/auth/SignupScreen.js';
 
+
 const AuthenticationStack = createStackNavigator(
   {
     Login: {
