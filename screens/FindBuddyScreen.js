08cee00814602d5ac6692fd87ab95dbdd8779fcf
jmkchoi <jstnch24@gmail.com>
2019-03-21 17:00:34 -0700

    add profile details to card

--- screens/FindBuddyScreen.js
+++ screens/FindBuddyScreen.js
@@ -14,19 +14,46 @@ import { WebBrowser } from 'expo';
 import { MonoText } from '../components/StyledText';
 const users = [
   { id: '1',
-    uri: 'http://www.availableideas.com/wp-content/uploads/2016/02/Dog-bites-canvas-shoes-iPhone-6-Wallpaper.jpg'
+    image: 'http://www.availableideas.com/wp-content/uploads/2016/02/Dog-bites-canvas-shoes-iPhone-6-Wallpaper.jpg',
+    name: {
+      first: 'Dog',
+      last: 'Alpha'
+    },
+    location: '90025',
+    activities: ['Running', 'Fetch'],
+    hours: 'Afternoon',
+    aboutMe: 'Woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof woof',
+    gym: 'the park'
   },
   { id: '2',
-    uri: 'https://iphonewalls.net/wp-content/uploads/2015/01/Cute%20Pug%20Dog%20Laughing%20iPhone%206%20Plus%20HD%20Wallpaper-320x480.jpg'
+    image: 'https://iphonewalls.net/wp-content/uploads/2015/01/Cute%20Pug%20Dog%20Laughing%20iPhone%206%20Plus%20HD%20Wallpaper-320x480.jpg',
+    name: {
+      first: 'Dawg',
+      last: 'Beta'
+    },
+    location: '90025',
+    activities: ['Walking', 'Biting'],
+    hours: 'Afternoon',
+    aboutMe: 'I\'m a dog',
+    gym: 'beach'
   },
   { id: '3',
-    uri: 'http://www.ohlays.com/wallpapers/dog1.jpg'
+    image: 'http://www.ohlays.com/wallpapers/dog1.jpg',
+    name: {
+      first: 'Air',
+      last: 'Bud'
+    },
+    location: '90025',
+    activities: ['Jumping', 'Basketball'],
+    hours: 'Afternoon',
+    aboutMe: 'Bark bark',
+    gym: 'Staples Center'
   },
 ];
 
 export default class FindBuddyScreen extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     
     this.position = new Animated.ValueXY();
     this.state = {
@@ -66,22 +93,50 @@ export default class FindBuddyScreen extends React.Component {
 
   renderUsers = () => {
     return users.map((item, i) => {
+      let activitiesLength = item.activities.length;
       if (i < this.state.currentIndex) {
         return null;
       } else if (i === this.state.currentIndex) {
         return (
           <Animated.View 
             {...this.PanResponder.panHandlers} 
-            key={item.id} style={[{transform: this.position.getTranslateTransform()}, {height: screen_height - 120, width: screen_width, padding: 10, position: 'absolute'}]}>
-            <Image style={styles.userImage} source={{uri: item.uri}}>
+            key={item.id} style={[styles.animatedContainer, {transform: this.position.getTranslateTransform()}]} >
+
+            <View style={styles.cardHeader}>
+              <Text style={styles.headerText}>
+                {item.name.first} {item.name.last[0]}.
+              </Text>
+            </View>
+
+            <Image style={styles.userImage} source={{uri: item.image}}>
             </Image>
+
+            <View style={styles.userDetails}>
+              <Text style={styles.profileText}>
+                {item.aboutMe}
+              </Text>
+              <Text style={styles.profileText}>
+                Activities: {item.activities.map((activity, i) => {
+                  if (i < activitiesLength - 1) {
+                    activity += ', ';
+                  }
+                return activity})}
+              </Text>
+              <Text style={styles.profileText}>
+                Preferred Time: {item.hours}s
+              </Text>
+              <Text style={styles.profileText}>
+                Preferred Location: {item.gym}
+              </Text>
+            </View>
+
           </Animated.View>
         );
       } else {
         return (
           <Animated.View 
-            key={item.id} style={[{height: screen_height - 120, width: screen_width, padding: 10, position: 'absolute'}]}>
-            <Image style={styles.userImage} source={{uri: item.uri}}>
+            key={item.id} style={[styles.animatedContainer, {padding: 0}]}>
+            <Image style={styles.blurredImage} source={{uri: item.image}} blurRadius={30}>
             </Image>
           </Animated.View>
         );
@@ -121,48 +176,61 @@ const screen_width = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#F5F5DC',
   },
   topContainer: {
-    height: 40
+    height: 80,
+    marginBottom: 100
   },
   bottomContainer: {
-    height: 100
+    height: 200
   },
   innerContainer: {
     flex: 1,
-    zIndex: 10
+    zIndex: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  userDetails: {
+    position: 'relative',
+    marginTop: 20,
+    marginBottom: 20
   },
   animatedContainer: {
-    height: screen_height - 300,
-    width: screen_width,
-    backgroundColor: 'white'
+    borderColor: '#FFF',
+    borderWidth: 2,
+    borderRadius: 20,
+    height: screen_height - 200,
+    width: screen_width - 50,
+    backgroundColor: '#FFF',
+    padding: 10,
+    position: 'absolute',
+    borderRadius: 20,
   },
   userImage: {
     flex: 1,
-    height: null,
+    height: '50%',
     width: null,
     resizeMode: 'cover',
-    padding: 10,
+    padding: 20,
     borderRadius: 20
   },
-  text: {
+  blurredImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    borderRadius: 20
+  },
+  headerText: {
+    fontSize: 30,
+    marginTop: 20,
     marginBottom: 20,
+    textAlign: 'center'
+  },
+  profileText: {
+    marginTop: 10,
     color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
+    fontSize: 20,
     lineHeight: 19,
     textAlign: 'center',
   },
-  imageContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  image: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  }
 });
