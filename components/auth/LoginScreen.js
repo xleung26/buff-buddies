ca8b4cae7cdcca436446b0128119cbb25ebe49f0
Gabriella Pernama <gabypernama@gmail.com>
2019-03-21 17:14:49 -0700

    commented out necessary code

--- components/auth/LoginScreen.js
+++ components/auth/LoginScreen.js
@@ -1,57 +1,62 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
-import emailLogin from './authHelpers.js';
+// import emailLogin from './authHelpers.js';
 
 export default class LoginScreen extends Component {
   static navigationOptions = {
-    title: 'Please Login'
+    title: "Please Login"
   };
   state = {
-    email: '', password: '',
+    email: "",
+    password: "",
     errorMessage: null
-  }
-
+  };
 
   // Login user with firebase
   _login = () => {
-    console.log('I know what Im doing');
+    console.log("I know what Im doing");
 
     // On successful login
     // Navigate to main function call passing user or setting user in config
     // On login failure
     // Trigger alert and ask to create account or try again
-    if (this.state.email !== null && this.state.email !== '' && this.state.password !== null && this.state.password !== '') {
-      this.props.navigation.navigate('Main');
+    if (
+      this.state.email !== null &&
+      this.state.email !== "" &&
+      this.state.password !== null &&
+      this.state.password !== ""
+    ) {
+      this.props.navigation.navigate("Main");
       // emailLogin(this.state.email, this.state.password);
     } else {
       // Alert user that they are missing a email or password
     }
-  }
+  };
   // This method will push the 'signup' view onto the stack
   _navigateToSignUp = () => {
-    this.props.navigation.navigate('Signup', {});
-  }
+    this.props.navigation.navigate("Signup", {});
+  };
 
   // This method will push the 'Main' view onto the stack
   _navigateToMain = () => {
-    this.props.navigation.navigate('Main', {});
-  }
+    this.props.navigation.navigate("Main", {});
+  };
 
   render() {
     return (
       <View
         style={{
-          flexDirection: 'column',
+          flexDirection: "column",
           height: 100,
           padding: 20,
-          backgroundColor: ''
-        }}>
+          backgroundColor: ""
+        }}
+      >
         <Text>Login</Text>
-        {this.state.errorMessage &&
-          <Text style={{ color: 'red' }}>
-            {this.state.errorMessage}
-          </Text>}
+        {this.state.errorMessage && (
+          <Text style={{ color: "red" }}>{this.state.errorMessage}</Text>
+        )}
         <TextInput
           style={styles.textInput}
           autoCapitalize="none"
@@ -75,12 +80,13 @@ export default class LoginScreen extends Component {
         />
         <View
           style={{
-            flexDirection: 'row',
+            flexDirection: "row",
             height: 100,
             padding: 20,
-            backgroundColor: '',
-            justifyContent: 'baseline'
-          }}>
+            backgroundColor: "",
+            justifyContent: "baseline"
+          }}
+        >
           <Text>Dont have an account yet?</Text>
           <Button
             onPress={this._navigateToSignUp}
@@ -98,14 +104,14 @@ export default class LoginScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   textInput: {
     height: 40,
-    width: '90%',
-    borderColor: 'gray',
+    width: "90%",
+    borderColor: "gray",
     borderWidth: 1,
     marginTop: 8
   }
-})
+});
